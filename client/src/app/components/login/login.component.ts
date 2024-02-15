ec2c89a6fe881d7d629533a6b6d7e425ec544481
Shlomi yechia <shlomiyechia@Shlomis-MacBook-Pro.local>
2024-02-15 13:59:18 +0200

    client side

---
+++ client/src/app/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  imageUrl: string = 'https://www.peakpx.com/en/hd-wallpaper-desktop-kqeiw';
+}
