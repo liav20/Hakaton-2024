c1b7f2280c3cceb8ec1171375c40d03372bc0bd1
DonKorazon <danikor187@gmail.com>
2024-02-15 18:05:12 +0200

    SC

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent}, 
+    {path: '', component: LoginComponent},
+    {path: 'login', component: LoginComponent}, 
     {path: 'register', component: RegisterComponent} 
 ];
