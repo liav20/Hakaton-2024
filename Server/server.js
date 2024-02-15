b41107c4b7d474753884229a641fa226dcb25dab
Ofir Sabag <sabagofir1@gmail.com>
2024-02-15 14:35:09 +0200

    added db connection

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const PORT = 3000;
+
+
+//connect the sql database
+const db = new sqlite3.Database("C:\\Users\\user\\Desktop\\mffDB", (err) => {
+    if (err) {
+        console.error('Error connecting to database:', err.message);
+    } else {
+        console.log('Connected to the database');
+        // Create tables or perform other initialization tasks here if needed
+    }
+});
 
 // Define a route
 app.get('/', (req, res) => {
