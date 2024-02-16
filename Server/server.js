344384d27fa60742ed83eee87d355f7e10748520
Ofir Sabag <sabagofir1@gmail.com>
2024-02-16 12:39:14 +0200

    added db connection and creation

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 const userRoute = require('./routes/userRoutes');
 const authRoute = require('./routes/authRoutes');
 const gameRoute = require('./routes/gameRoutes');
+
+
+//connect the sql database
+const db = new sqlite3.Database("C:\\Users\\user\\Desktop\\mffDB", (err) => {
+    if (err) {
+        console.error('Error connecting to database:', err.message);
+    } else {
+        console.log('Connected to the database');
+        // Create tables or perform other initialization tasks here if needed
+    }
+});
+
 
 // Use routes
 app.use('/api/friend', userRoute);
