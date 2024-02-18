ac34f7a0ea846ab7333a53fabfba82e5f2bad995
DonKorazon <danikor187@gmail.com>
2024-02-18 20:15:41 +0200

    hostId

--- client/src/app/components/host-screen/host-screen.component.ts
+++ client/src/app/components/host-screen/host-screen.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InviteScreenComponent } from '../invite-screen/invite-screen.component';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-host-screen',
   standalone: true,
   imports: [
     CommonModule,
-    InviteScreenComponent
-    ],
+    InviteScreenComponent,
+    GameService],
   templateUrl: './host-screen.component.html',
   styleUrl: './host-screen.component.css'
 })
 export class HostScreenComponent {
+  constructor(private _gameService: GameService){}
 
+  func(){
+    alert();
+    console.log(this._gameService.getGameId());
+  }
 }
