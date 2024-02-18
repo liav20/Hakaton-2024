2f6d5ca13802d2bc2000107f88241b5b3f8cbe0f
Ofir Sabag <sabagofir1@gmail.com>
2024-02-18 10:36:11 +0200

    added game model

---
+++ Server/models/Game.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Scheme({
+    HostID: {
+        require: false,
+        type: String
+    },
+    WinningTeam: {
+        require: false,
+        type: Array
+    },
+    LosingTeam: {
+        require: false,
+        type: Array
+    },
+    WinningScore: {
+        require: false,
+        type: Number
+    },
+    LosingScore: {
+        require: false,
+        type: Number
+    },
+    Date: {
+        require: false,
+        type: Date
+    }
+})
+
+module.exports = mongoose.model('Game', dataSchema);
