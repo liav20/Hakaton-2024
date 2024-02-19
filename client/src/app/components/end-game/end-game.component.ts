13aa4f5bced9c43ab5f1663d7ee7dfb44349203f
DonKorazon <danikor187@gmail.com>
2024-02-19 23:09:51 +0200

    service and end-game components

---
+++ client/src/app/components/end-game/end-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './end-game.component.html',
+  styleUrl: './end-game.component.css'
+})
+export class EndGameComponent {
+  
+}
