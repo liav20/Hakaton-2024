e6b8ab94e473967b1d6a6af906a4a8b2f5bea570
DonKorazon <danikor187@gmail.com>
2024-02-19 22:13:29 +0200

    score screen need to css

--- client/src/app/components/score-screen/score-screen.component.ts
+++ client/src/app/components/score-screen/score-screen.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { TimerComponent } from '../timer/timer.component';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-score-screen',
   standalone: true,
-  imports: [TimerComponent],
+  imports: [TimerComponent, FormsModule],
   templateUrl: './score-screen.component.html',
   styleUrl: './score-screen.component.css'
 })
 export class ScoreScreenComponent {
+  teamAGoals: number = 1;
+  teamBGoals: number = 1;
+  teamA: string = 'Daniel';
+  teamB: string = 'Shlomi';
 
+  constructor() { }
+
+  decreaseGoal(team: string) {
+    if (team === 'A' && this.teamAGoals > 0) {
+      this.teamAGoals--;
+    } else if (team === 'B' && this.teamBGoals > 0) {
+      this.teamBGoals--;
+    }
+  }
+
+  increaseGoal(team: string) {
+    if (team === 'A') {
+      this.teamAGoals++;
+    } else if (team === 'B') {
+      this.teamBGoals++;
+    }
+  }
 }
